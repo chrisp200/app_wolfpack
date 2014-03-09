a4e71d6603b4f494ccb8ce8fe7a297fc03041196
nathanmccartney-macbook <nmccartney1231@gmail.com>
2014-03-09 13:09:41 -0400

    added assets

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -12,7 +12,7 @@ var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.d
         $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
     })
     .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'http://www.wolfpack.io', controller: 'HomeCtrl'});
+        $routeProvider.when('/', {templateUrl: 'partials/homeView.html', controller: 'HomeCtrl'});
         $routeProvider.when('/view1', {templateUrl: 'partials/notificationView.html'});
         $routeProvider.when('/view2', {templateUrl: 'partials/geolocationView.html'});
         $routeProvider.when('/view3', {templateUrl: 'partials/accelerometerView.html'});
